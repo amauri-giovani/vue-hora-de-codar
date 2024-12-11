e0b1e470a0f29dc0c6a71e0b6f2e9bfd840ec1c1
Amauri Rossetti Giovani <amauri.giovani@gmail.com>
2024-12-10 21:37:53 -0300

    Changed github account

---
+++ 2_projeto_vue/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Header :esta_logado="false" />
+    <PrimeiroComponente />
+    <LifeCycle />
+    <Pessoa />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+import LifeCycle from "./components/LifeCycle.vue";
+import Pessoa from "./components/Pessoa.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    PrimeiroComponente,
+    LifeCycle,
+    Pessoa
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: #333;
+  color: #FFF;
+}
+a {
+  color: red;
+}
+.teste {
+  background-color: #000;
+}
+</style>
---
+++ 2_projeto_vue/src/components/Pessoa.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <h2>Esta é a descrição da pessoa {{ nome }}</h2>
+    <Info :email="email" :esta_trabalhando="true" :cargo="cargo" />
+    <Form />
+</div>
+</template>
+
+<script>
+import Info from './Info.vue';
+import Form from './Form.vue';
+
+export default {
+    name: "Pessoa",
+    components: {
+    Info,
+    Form
+},
+    data() {
+        return {
+            nome: "Amauri",
+            email: 'amauri@email.com',
+            cargo: 'Desenvolvedor de Sistemas'
+        };
+    },
+};
+</script>
